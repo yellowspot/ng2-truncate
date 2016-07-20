577af3f4b0110a3374290e6843f8099b935b889d
Cristian Stügelmayer <cstugelmayer@yellowspot.com.ar>
2016-07-20 11:22:38 -0300

    Upcase to the class name

--- src/ng2-truncate.pipe.ts
+++ src/ng2-truncate.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe } from '@angular/core';
 @Pipe({
   name: 'truncate'
 })
-export class ng2TruncatePipe {
+export class Ng2TruncatePipe {
   transform(value: string, limit: number = 40, trail: String = '...') : string {
     return value.length > limit ? value.substring(0, limit) + trail : value;
   }
