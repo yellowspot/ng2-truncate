f766d1a1b02a88ca96c23839e7d3ba9cd2d7195a
Cristian Stügelmayer <cstugelmayer@yellowspot.com.ar>
2016-07-24 19:49:06 -0300

    Add truncate directive

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { TruncateCharactersPipe } from './truncate-characters.pipe'
 import { TruncateWordsPipe } from './truncate-words.pipe'
+import { NgTruncateComponent } from './components/truncate-characters.component'
 
 export const TRUNCATE_PIPES = [TruncateCharactersPipe, TruncateWordsPipe]
+export const TRUNCATE_DIRECTIVES = [NgTruncateComponent]
 
 export default {
-  pipes: TRUNCATE_PIPES
+  pipes: TRUNCATE_PIPES,
+  directives: TRUNCATE_DIRECTIVES
 }
