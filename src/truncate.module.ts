021afd07deee0b3e39bd3341739b0f2d2327cef3
Cristian Stügelmayer <cstugelmayer@yellowspot.com.ar>
2016-11-05 18:58:45 -0300

    New index.ts

--- src/truncate.module.ts
+++ src/truncate.module.ts
@@ -4,7 +4,8 @@ import { TruncateCharactersPipe } from './truncate-characters.pipe'
 import { TruncateWordsPipe } from './truncate-words.pipe'
 
 @NgModule({
-  declarations: [TruncateCharactersPipe, TruncateWordsPipe]
+  declarations: [TruncateCharactersPipe, TruncateWordsPipe],
+  exports: [TruncateCharactersPipe, TruncateWordsPipe]
 })
 
 export class TruncateModule { }
