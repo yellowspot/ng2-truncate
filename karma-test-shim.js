4571049e65141201dec4f7bf18a3de23b58b4313
Matias Grunberg <matias@yellowspot.com.ar>
2017-01-17 23:15:28 -0300

    Replace systemjs with webpack on karma / specs

--- karma-test-shim.js
+++ karma-test-shim.js
@@ -1,58 +1,24 @@
-// Turn on full stack traces in errors to help debugging
-Error.stackTraceLimit=Infinity;
+Error.stackTraceLimit = Infinity;
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 100;
+require('core-js/es6');
+require('reflect-metadata');
+require('rxjs');
+require('zone.js/dist/zone');
+require('zone.js/dist/long-stack-trace-zone');
+require('zone.js/dist/async-test');
+require('zone.js/dist/fake-async-test');
+require('zone.js/dist/sync-test');
+require('zone.js/dist/proxy');
+require('zone.js/dist/jasmine-patch');
 
-// Cancel Karma's synchronous start,
-// we will call `__karma__.start()` later, once all the specs are loaded.
-__karma__.loaded = function() {};
+var appContext = require.context('./src', true, /\.spec\.ts/);
 
-System.config({
-    baseURL: '/base/',
-    defaultJSExtensions: true,
-    map: {
-        '@angular': 'node_modules/@angular',
-        'rxjs': 'node_modules/rxjs'
-    },
-    packages: {
-        '@angular/common': { main: 'index.js', defaultExtension: 'js' },
-        '@angular/compiler': { main: 'index.js', defaultExtension: 'js' },
-        '@angular/core': { main: 'index.js', defaultExtension: 'js' },
-        '@angular/platform-browser': { main: 'index.js', defaultExtension: 'js' }
-    }
-});
+appContext.keys().forEach(appContext);
 
-System.import('@angular/platform-browser/src/browser/browser_adapter').then(function(browser_adapter) {
-    browser_adapter.BrowserDomAdapter.makeCurrent();
-}).then(function() {
-        return Promise.all(
-            Object.keys(window.__karma__.files) // All files served by Karma.
-                .filter(onlySpecFiles)
-                .map(file2moduleName)
-                .map(function(path) {
-                    return System.import(path).then(function(module) {
-                        if (module.hasOwnProperty('main')) {
-                            module.main();
-                        }
-                    });
-                }));
-    })
-    .then(function() {
-        __karma__.start();
-    }, function(error) {
-        console.error(error.message || error);
-        console.error(error.stack || error);
-        __karma__.start();
-    });
+var testing = require('@angular/core/testing');
+var browser = require('@angular/platform-browser-dynamic/testing');
 
-
-function onlySpecFiles(path) {
-    return /[\.|_]spec\.js$/.test(path);
-}
-
-// Normalize paths to module names.
-function file2moduleName(filePath) {
-    return filePath.replace(/\\/g, '/')
-        .replace(/^\/base\//, '')
-        .replace(/\.js/, '');
-}
+testing.getTestBed().initTestEnvironment(
+  browser.BrowserDynamicTestingModule,
+  browser.platformBrowserDynamicTesting()
+);
