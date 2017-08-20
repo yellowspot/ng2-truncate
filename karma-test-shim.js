7c1ba2bb56bd25be950b979b8c62abcfbe32fc0c
Cristian Stügelmayer <cstugelmayer@yellowspot.com.ar>
2017-08-20 14:59:15 -0300

    Update karma config

--- karma-test-shim.js
+++ karma-test-shim.js
@@ -1,24 +1,29 @@
-Error.stackTraceLimit = Infinity;
+import 'core-js';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
+import 'zone.js/dist/proxy.js';
+import 'zone.js/dist/sync-test';
+import 'zone.js/dist/jasmine-patch';
+import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
 
-require('core-js/es6');
-require('reflect-metadata');
-require('rxjs');
-require('zone.js/dist/zone');
-require('zone.js/dist/long-stack-trace-zone');
-require('zone.js/dist/async-test');
-require('zone.js/dist/fake-async-test');
-require('zone.js/dist/sync-test');
-require('zone.js/dist/proxy');
-require('zone.js/dist/jasmine-patch');
+import { getTestBed } from '@angular/core/testing';
+import {
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
 
-var appContext = require.context('./src', true, /\.spec\.ts/);
+import 'rxjs';
 
-appContext.keys().forEach(appContext);
-
-var testing = require('@angular/core/testing');
-var browser = require('@angular/platform-browser-dynamic/testing');
-
-testing.getTestBed().initTestEnvironment(
-  browser.BrowserDynamicTestingModule,
-  browser.platformBrowserDynamicTesting()
+getTestBed().initTestEnvironment(
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting()
 );
+
+const testContext = require.context('./src', true, /\.spec\.ts/);
+
+function requireAll(requireContext) {
+    return requireContext.keys().map(requireContext);
+}
+
+const modules = requireAll(testContext);
