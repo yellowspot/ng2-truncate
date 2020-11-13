41c0446964219aa18a0d2535a2cf333f129a59d4
Cristian Stügelmayer <cristian@yellowspot.dev>
2020-11-13 12:26:31 -0300

    specs refactor

--- projects/yellowspot/ng-truncate/src/lib/truncate-characters.pipe.spec.ts
+++ projects/yellowspot/ng-truncate/src/lib/truncate-characters.pipe.spec.ts
@@ -12,16 +12,8 @@ describe('TruncateCharactersPipe', () => {
     expect(pipe.transform('123456789', 4)).toEqual('1234…');
   });
 
-  it('transforms "123456789" to "123… when including trailing character"', () => {
-    expect(pipe.transform('123456789', 4, undefined, true)).toEqual('123…');
-  });
-
   it('transforms "123456789" to "1234xxx"', () => {
     expect(pipe.transform('123456789', 4, 'xxx')).toEqual('1234xxx');
-  });
-
-  it('transforms "123456789" to "12xxx when including trailing character"', () => {
-    expect(pipe.transform('123456789', 4, 'xxx', true)).toEqual('1xxx');
   });
 
   it('transforms "1234 5678" to "123…"', () => {
@@ -32,16 +24,8 @@ describe('TruncateCharactersPipe', () => {
     expect(pipe.transform('123', 3)).toEqual('123');
   });
 
-  it('change "123" to "12… if including trailing char"', () => {
-    expect(pipe.transform('123', 3, undefined, true)).toEqual('12…');
-  });
-
   it('leaves "12" unchanged', () => {
     expect(pipe.transform('12', 3)).toEqual('12');
-  });
-
-  it('leaves "12" unchanged when including trailing char', () => {
-    expect(pipe.transform('12', 3, undefined, true)).toEqual('12');
   });
 
   it('leaves empty string unchanged', () => {
@@ -53,23 +37,41 @@ describe('TruncateCharactersPipe', () => {
     expect(pipe.transform('123456789', -4, '…')).toEqual('…6789');
   });
 
-  it('[left] position including trailing', () => {
-    expect(pipe.transform('123456789', -4, '…', true)).toEqual('…789');
-  });
-
   it('[left] leaves empty string unchanged', () => {
     expect(pipe.transform('', -3, '…')).toEqual('');
-  });
-
-  it('[left] leaves empty string unchanged when including trailing', () => {
-    expect(pipe.transform('', -3, '…', true)).toEqual('');
   });
 
   it('do not throw if undefined provided', () => {
     expect(pipe.transform(undefined, 3)).toEqual('');
   });
 
-  it('do not throw if undefined provided when include trailing', () => {
-    expect(pipe.transform(undefined, 3, undefined, true)).toEqual('');
+  describe('when including trailing char', () => {
+    it('transforms "123456789" to "123…"', () => {
+      expect(pipe.transform('123456789', 4, undefined, true)).toEqual('123…');
+    });
+
+    it('transforms "123456789" to "12xxx"', () => {
+      expect(pipe.transform('123456789', 4, 'xxx', true)).toEqual('1xxx');
+    });
+
+    it('change "123" to "12…"', () => {
+      expect(pipe.transform('123', 3, undefined, true)).toEqual('12…');
+    });
+
+    it('leaves "12" unchanged', () => {
+      expect(pipe.transform('12', 3, undefined, true)).toEqual('12');
+    });
+
+    it('[left] position', () => {
+      expect(pipe.transform('123456789', -4, '…', true)).toEqual('…789');
+    });
+
+    it('[left] leaves empty string unchanged', () => {
+      expect(pipe.transform('', -3, '…', true)).toEqual('');
+    });
+
+    it('do not throw if undefined provided', () => {
+      expect(pipe.transform(undefined, 3, undefined, true)).toEqual('');
+    });
   });
 });
